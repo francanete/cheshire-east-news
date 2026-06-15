e970c438c62b2e11e84aab34bb7d7bdc1d373ae4
Fran <felx.assistant@gmail.com>
2026-06-15 16:14:44 +0000

    Initial Astro news site

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://francanete.github.io/cheshire-east-news',
+  base: '/cheshire-east-news/',
+  output: 'static',
+});
